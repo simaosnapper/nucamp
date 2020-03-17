65cc619e776ad2c082070b6244a92ff54508678a
Corritore, Ian <corritorei@upmc.edu>
2020-03-17 02:15:56 -0400

    react week 2

---
+++ react/nucampsite/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+import { CAMPSITES } from './shared/campsites';
+
+import Directory from './components/Directory.component';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='/'>NuCamp</NavbarBrand>
+          </div>
+        </Navbar>
+        <Directory campsites={this.state.campsites}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/nucampsite/src/components/Directory.component.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle
+} from 'reactstrap';
+
+import CampsiteInfo from './CampsiteInfo.component';
+
+class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  onCampsiteSelect(campsite) {
+    this.setState({ selectedCampsite: campsite });
+  }
+
+  render() {
+    const directory = this.props.campsites.map(campsite => {
+      return (
+        <div key={campsite.id} className='col-md-5 m-1'>
+          <Card onClick={() => this.onCampsiteSelect(campsite)}>
+            <CardImg width='100%' src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+              <CardTitle className="text-center"><h5>{campsite.name}</h5></CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+
+    return (
+      <div className='container'>
+        <div className='row'>{directory}</div>
+        <CampsiteInfo campsite={this.state.selectedCampsite} />
+      </div>
+    );
+  }
+}
+
+export default Directory;
