01da1344926b22448e6cb7e9008d78207771189d
Corritore, Ian <corritorei@upmc.edu>
2020-05-06 02:33:48 -0400

    week 3

--- react-native/nucampsite/App.js
+++ react-native/nucampsite/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 import { YellowBox } from 'react-native';
+import Main from './components/MainComponent';
+
+const { persistor, store } = ConfigureStore();
 
 YellowBox.ignoreWarnings(['Warning: ...']);
 console.disableYellowBox = true;
 
-const store = ConfigureStore();
-
 export default function App() {
     return (
         <Provider store={store}>
-            <Main />
+            <PersistGate
+                loading={<Loading />}
+                persistor={persistor}>
+                <Main />
+            </PersistGate>
         </Provider>
     );
 }
