7c9e798b829d0a7dd4efd499acd3a6381494a52f
Ian Corritore <iancorritore@ians-mbp.myfiosgateway.com>
2020-10-24 00:37:04 -0400

    react-native2 week 2 workshop

--- react-native2/nucampsite/components/AboutComponent.js
+++ react-native2/nucampsite/components/AboutComponent.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { ScrollView, FlatList, Text } from 'react-native';
-import { PARTNERS } from '../shared/partners';
-import { ListItem, Card } from 'react-native-elements';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 const Mission = () => (
     <Card
@@ -15,33 +23,51 @@ const Mission = () => (
     </Card>
 );
 
-export default class AboutComponent extends Component {
+class About extends Component {
     static navigationOptions = {
         title: 'About Us'
     };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            PARTNERS
-        };
-    }
-
     render() {
-        const renderPartner = ({ item }) => (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
-            />
-        );
+        const renderPartner = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
+                />
+            );
+        };
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
         return (
             <ScrollView>
                 <Mission />
                 <Card
                     title={'Community Partners'}>
                     <FlatList 
-                        data={this.state.PARTNERS}
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     />
@@ -50,3 +76,5 @@ export default class AboutComponent extends Component {
         );
     }
 }
+
+export default connect(mapStateToProps)(About);
--- react/json-server/readme.md
+++ react/json-server/readme.md
@@ -5,3 +5,10 @@ json-server --watch db.json -p 3001 -d 2000 -H 10.0.0.24
  - Mac
 
 json-server --watch db.json -p 3001 -d 2000 -H 10.0.0.160
+
+-- new
+(works on mac)
+
+json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000
+
+-- mac machine ip - 192.168.1.163
